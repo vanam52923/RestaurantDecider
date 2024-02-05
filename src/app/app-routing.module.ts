0235a53953e327c8cbd580fb6f39d37794012314
Manav <874049@cognizant.com>
2024-02-06 02:35:34 +0800

    Init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'/login', pathMatch:'full'},
+  {path: 'restaurant/:sessionId', component:RestaurantComponent},
+  {path: 'login', component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
